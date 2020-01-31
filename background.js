731a3aeef68f8c5e4dee91dd96a4c3c99409eb6d
nicshackle <nicshackle@gmail.com>
2020-01-31 10:15:44 +0200

    Update background.js

--- background.js
+++ background.js
@@ -7,15 +7,28 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     var uuids = url.match(/[0-9]{13}\-[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/g)
     if(uuids){
     	var uuid = uuids[0]
+
 	    input.setAttribute('value', uuid);
 	    document.body.appendChild(input);
 	    input.select();
 	    var result = document.execCommand('copy');
 	    document.body.removeChild(input);
-	    alert("Copied " + uuid)
+
+	    var x = document.createElement("P");
+	    x.style.position = 'absolute'; 
+	    x.style.backgroundColor = 'yellow';
+	    x.style.padding = '8px';
+	    x.style.margin = '10px'
+		var t = document.createTextNode("Copied: "+uuid);
+		x.appendChild(t);
+		document.body.appendChild(x); 
+		setTimeout(()=>{
+			document.body.removeChild(x); 
+		}, 3000)   
 	} else {
 		alert("No Yoco UUID found in this URL")
 	}
  	`
   });
 });
+
